86d6fdbe51887e64542756b721aa6c98129f88f4
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2022-07-17 17:10:25 -0300

    CRUD with Bootstrap

--- Web-Application/Controllers/FabricantesController.cs
+++ Web-Application/Controllers/FabricantesController.cs
@@ -102,6 +102,7 @@ namespace Web_Application.Controllers
             Fabricante fabricante = context.Fabricantes.Find(id);
             context.Fabricantes.Remove(fabricante);
             context.SaveChanges();
+            TempData["Message"] = "Fabricante " + fabricante.Nome.ToUpper() + " foi removido";
             return RedirectToAction("Index");
         }
     }
